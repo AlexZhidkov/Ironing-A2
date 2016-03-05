cda9d98d3bbd7fb395aa8e81809a1e10255e5fce
Alexander Zhidkov <azhidkov@gmail.com>
2016-03-05 15:40:18 +0800

    FormBuilder

--- src/components/order-form/order-form.ts
+++ src/components/order-form/order-form.ts
@@ -1,17 +1,32 @@
 import { Component } from 'angular2/core';
 import { CanActivate } from 'angular2/router';
 import { AuthRouteHelper } from 'core/auth/auth-route-helper';
+import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
 
 const template: string = require('./order-form.html');
 
 
 @Component({
-  selector: 'orderForm',
-  template
+    selector: 'orderForm',
+    template
 })
 
 @CanActivate(() => AuthRouteHelper.requireAuth())
 
 export class OrderForm {
-  constructor() {}
+    order: ControlGroup;
+    builder: FormBuilder;
+    constructor(fb: FormBuilder) {
+        this.order = fb.group({
+            'name': ['', Validators.required],
+            'email': ['', Validators.required],
+            'phone': ['', Validators.required],
+            'address': ['', Validators.required],
+            'message': ['', Validators.required]
+        });
+    }
+    submit(event: any): void {
+        console.log(this.order.value);
+        event.preventDefault();
+    }
 }
