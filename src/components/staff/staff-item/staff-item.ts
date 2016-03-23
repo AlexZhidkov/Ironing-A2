de104d7aa398ed24ad9c577c5c630857ad33936d
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 11:55:45 +0800

    UserService

--- src/components/staff/staff-item/staff-item.ts
+++ src/components/staff/staff-item/staff-item.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from 'angular2/core';
-import { IStaff } from 'core/staff/staff';
-import { StaffService } from 'core/staff/staff-service';
+import { IUser } from 'core/user/user';
+import { UserService } from 'core/user/user-service';
 import { Autofocus } from 'directives/autofocus-directive';
 
 const styles: string = require('./staff-item.scss');
@@ -8,26 +8,26 @@ const template: string = require('./staff-item.html');
 
 
 @Component({
-  directives: [
-    Autofocus
-  ],
-  selector: 'staff-item',
-  styles: [styles],
-  template
+    directives: [
+        Autofocus
+    ],
+    selector: 'staff-item',
+    styles: [styles],
+    template
 })
 
 export class StaffItem {
-  @Input() model: IStaff;
+    @Input() model: IUser;
 
-  constructor(private staffService: StaffService) {}
+    constructor(private userService: UserService) { }
 
-  delete(): void {
-    this.staffService.deleteStaff(this.model);
-  }
+    delete(): void {
+        this.userService.deleteUser(this.model);
+    }
 
-  setRole(role: string): void {
-    this.model.role = role;
-    this.staffService.updateStaff(this.model.key, {role: role});
-  }
+    setRole(role: string): void {
+        this.model.role = role;
+        this.userService.updateUser(this.model.key, { role: role });
+    }
 
 }
