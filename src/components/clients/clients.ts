54b5680b07f93d69a22bfe918ef52d4fc96388ae
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 13:33:59 +0800

    UserStore

--- src/components/clients/clients.ts
+++ src/components/clients/clients.ts
@@ -1,22 +1,22 @@
 import { Component } from 'angular2/core';
 import { CanActivate } from 'angular2/router';
 import { AuthRouteHelper } from 'core/auth/auth-route-helper';
-import { ClientStore } from 'core/client/client-store';
+import { UserStore } from 'core/user/user-store';
 import { ClientList } from './client-list/client-list';
 
 const template: string = require('./clients.html');
 
 
 @Component({
-  directives: [
-    ClientList
-  ],
-  selector: 'clients',
-  template
+    directives: [
+        ClientList
+    ],
+    selector: 'clients',
+    template
 })
 
 @CanActivate(() => AuthRouteHelper.requireAuth() !== null)
 
 export class Clients {
-  constructor(public clientStore: ClientStore) {}
+    constructor(public userStore: UserStore) { }
 }
