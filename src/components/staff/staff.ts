01804cc2b4d3a249cf8a5f609281c9ff5e2cc90b
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 11:53:40 +0800

    UserStore

--- src/components/staff/staff.ts
+++ src/components/staff/staff.ts
@@ -1,22 +1,22 @@
 import { Component } from 'angular2/core';
 import { CanActivate } from 'angular2/router';
 import { AuthRouteHelper } from 'core/auth/auth-route-helper';
-import { StaffStore } from 'core/staff/staff-store';
+import { UserStore } from 'core/user/user-store';
 import { StaffList } from './staff-list/staff-list';
 
 const template: string = require('./staff.html');
 
 
 @Component({
-  directives: [
-    StaffList
-  ],
-  selector: 'staff',
-  template
+    directives: [
+        StaffList
+    ],
+    selector: 'staff',
+    template
 })
 
 @CanActivate(() => AuthRouteHelper.requireAuth() !== null)
 
 export class Staff {
-  constructor(public staffStore: StaffStore) {}
+    constructor(public userStore: UserStore) { }
 }
