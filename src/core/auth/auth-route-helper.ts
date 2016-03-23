4a5d717f636708c19ca115f08b3b34781f7c4329
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 13:47:54 +0800

    Update auth-route-helper.ts

--- src/core/auth/auth-route-helper.ts
+++ src/core/auth/auth-route-helper.ts
@@ -1,7 +1,7 @@
 import { Injectable } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { AuthService } from './auth-service';
-import { IUser } from './user';
+import { IUser } from './../user/user';
 
 /**
  * This is an ugly workaround until `CanActivate` supports DI
@@ -10,23 +10,23 @@ import { IUser } from './user';
 
 @Injectable()
 export class AuthRouteHelper {
-  static auth: AuthService;
-  static router: Router;
+    static auth: AuthService;
+    static router: Router;
 
-  static requireAuth(): IUser {
-    const { auth, router } = AuthRouteHelper;
-    if (!auth.authenticated) router.navigate(['/Sign In']);
-    return auth.authenticated;
-  }
+    static requireAuth(): IUser {
+        const { auth, router } = AuthRouteHelper;
+        if (!auth.authenticated) router.navigate(['/Sign In']);
+        return auth.authenticated;
+    }
 
-  static requireUnauth(): boolean {
-    const { auth, router } = AuthRouteHelper;
-    if (auth.authenticated) router.navigate(['/Order Form']);
-    return (auth.authenticated === null);
-  }
+    static requireUnauth(): boolean {
+        const { auth, router } = AuthRouteHelper;
+        if (auth.authenticated) router.navigate(['/Order Form']);
+        return (auth.authenticated === null);
+    }
 
-  constructor(auth: AuthService, router: Router) {
-    AuthRouteHelper.auth = auth;
-    AuthRouteHelper.router = router;
-  }
+    constructor(auth: AuthService, router: Router) {
+        AuthRouteHelper.auth = auth;
+        AuthRouteHelper.router = router;
+    }
 }
