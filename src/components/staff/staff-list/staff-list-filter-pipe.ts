aea48afd56ada292a38a5674eb50e452682218a4
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 11:55:01 +0800

    UserStore

--- src/components/staff/staff-list/staff-list-filter-pipe.ts
+++ src/components/staff/staff-list/staff-list-filter-pipe.ts
@@ -1,19 +1,19 @@
 import { Pipe, PipeTransform } from 'angular2/core';
-import { IStaff } from 'core/staff/staff';
+import { IUser } from 'core/user/user';
 
 
 @Pipe({
-  name: 'filterStaff',
-  pure: true
+    name: 'filterStaff',
+    pure: true
 })
 
 export class StaffListFilterPipe implements PipeTransform {
-  transform(list: IStaff[], filterType?: string[]): IStaff[] {
-    if (!list || !filterType) {
-      return list;
+    transform(list: IUser[], filterType?: string[]): IUser[] {
+        if (!list || !filterType) {
+            return list;
+        }
+        return list.filter((staff: IUser) => {
+            return staff.role === filterType[0];
+        });
     }
-    return list.filter((staff: IStaff) => {
-          return staff.role === filterType[0];
-    });
-  }
 }
