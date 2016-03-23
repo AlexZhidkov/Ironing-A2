e3fe7f8e5504d18244e0def04602bef1a3195f31
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-23 15:06:25 +0800

    Added ClientListFilterPipe

--- src/components/clients/client-list/client-list.ts
+++ src/components/clients/client-list/client-list.ts
@@ -3,24 +3,28 @@ import { RouterLink, RouteParams } from 'angular2/router';
 import { List } from 'immutable';
 import { ReplaySubject } from 'rxjs/subject/ReplaySubject';
 import { ClientItem } from '../client-item/client-item';
+import { ClientListFilterPipe } from './client-list-filter-pipe';
 
 const template: string = require('./client-list.html');
 const style: string = require('./client-list.scss');
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  directives: [
-    RouterLink,
-    ClientItem
-  ],
-  selector: 'client-list',
-  styles: [style],
-  template
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    directives: [
+        RouterLink,
+        ClientItem
+    ],
+    pipes: [
+        ClientListFilterPipe
+    ],
+    selector: 'client-list',
+    styles: [style],
+    template
 })
 
 export class ClientList {
-  @Input() clients: ReplaySubject<List<any>>;
+    @Input() clients: ReplaySubject<List<any>>;
 
-  constructor(params: RouteParams) {
-  }
+    constructor(params: RouteParams) {
+    }
 }
